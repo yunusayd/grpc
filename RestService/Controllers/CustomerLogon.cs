983a8a9c3bab3acb428f7fff433652f0b4e12ac1
yunusayd <yunusayd@gmail.com>
2021-02-12 18:58:17 +0300

    fix

--- RestService/Controllers/CustomerLogon.cs
+++ RestService/Controllers/CustomerLogon.cs
@@ -11,11 +11,6 @@ namespace RestService.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,16 +19,9 @@ namespace RestService.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return new[] {"asdasd", "azsdasd"};
         }
     }
 }
