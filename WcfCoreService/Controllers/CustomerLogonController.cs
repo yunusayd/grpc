8742cb567fb2e7e6d48b270676f2765c8b39d52a
yunus ayd <yunusayd@gmail.com>
2021-02-12 18:08:58 +0300

    Add files via upload

---
+++ WcfCoreService/Controllers/CustomerLogonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WcfCoreService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerLogonController : ControllerBase
+    {
+        private readonly ILogger<CustomerLogonController> _logger;
+
+        public CustomerLogonController(ILogger<CustomerLogonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+           return null;
+        }
+    }
+}
